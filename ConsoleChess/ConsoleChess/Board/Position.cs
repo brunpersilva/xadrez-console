a457a9d211491edde7ec3b257a74be1923ce1d46
Bruno Silva <brunpersilva@gmail.com>
2021-01-19 21:30:53 -0300

    Created Class Position

---
+++ ConsoleChess/ConsoleChess/Board/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace Board
+{
+    public class Position
+    {
+        public int Rows { get; set; }
+        public int Ranks { get; set; }
+
+        public Position(int rowns, int ranks)
+        {
+            Rows = rowns;
+            Ranks = ranks;
+        }
+
+        public override string ToString()
+        {
+            return Ranks + ", " + Rows;
+        }
+    }
+}
