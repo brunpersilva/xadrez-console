7e05a17e6353b3cc9b5e867e4dc7b8aae0e2d4c2
Bruno <brunpersilva@gmail.com>
2021-02-27 18:18:47 -0300

    Fixed a bug in the king class where the temporary variable for the position was called insteady of the actual piece position.

--- ConsoleChess/ConsoleChess/Chess/King.cs
+++ ConsoleChess/ConsoleChess/Chess/King.cs
@@ -29,87 +29,83 @@ namespace chess
         {
             bool[,] mat = new bool[Tab.Ranks, Tab.Files];
 
-            Position position = new Position(0, 0);
+            Position pos = new Position(0, 0);
 
             //up
-            position.DefineValues(PiecePosition.Rank - 1, PiecePosition.File);
-            if (Tab.ValidPosition(position) && CanMove(position))
+            pos.DefineValues(PiecePosition.Rank - 1, PiecePosition.File);
+            if (Tab.ValidPosition(pos) && CanMove(pos))
             {
-                mat[position.Rank, position.File] = true;
+                mat[pos.Rank, pos.File] = true;
             }
 
             //ne
-            position.DefineValues(PiecePosition.Rank - 1, PiecePosition.File + 1);
-            if (Tab.ValidPosition(position) && CanMove(position))
+            pos.DefineValues(PiecePosition.Rank - 1, PiecePosition.File + 1);
+            if (Tab.ValidPosition(pos) && CanMove(pos))
             {
-                mat[position.Rank, position.File] = true;
+                mat[pos.Rank, pos.File] = true;
             }
             //right
-            position.DefineValues(PiecePosition.Rank, PiecePosition.File + 1);
-            if (Tab.ValidPosition(position) && CanMove(position))
+            pos.DefineValues(PiecePosition.Rank, PiecePosition.File + 1);
+            if (Tab.ValidPosition(pos) && CanMove(pos))
             {
-                mat[position.Rank, position.File] = true;
+                mat[pos.Rank, pos.File] = true;
             }
 
             //se
-            position.DefineValues(PiecePosition.Rank + 1, PiecePosition.File + 1);
-            if (Tab.ValidPosition(position) && CanMove(position))
+            pos.DefineValues(PiecePosition.Rank + 1, PiecePosition.File + 1);
+            if (Tab.ValidPosition(pos) && CanMove(pos))
             {
-                mat[position.Rank, position.File] = true;
+                mat[pos.Rank, pos.File] = true;
             }
             //left
-            position.DefineValues(PiecePosition.Rank, PiecePosition.File - 1);
-            if (Tab.ValidPosition(position) && CanMove(position))
+            pos.DefineValues(PiecePosition.Rank, PiecePosition.File - 1);
+            if (Tab.ValidPosition(pos) && CanMove(pos))
             {
-                mat[position.Rank, position.File] = true;
+                mat[pos.Rank, pos.File] = true;
             }
             //down
-            position.DefineValues(PiecePosition.Rank + 1, PiecePosition.File);
-            if (Tab.ValidPosition(position) && CanMove(position))
+            pos.DefineValues(PiecePosition.Rank + 1, PiecePosition.File);
+            if (Tab.ValidPosition(pos) && CanMove(pos))
             {
-                mat[position.Rank, position.File] = true;
+                mat[pos.Rank, pos.File] = true;
             }
             //sw
-            position.DefineValues(PiecePosition.Rank + 1, PiecePosition.File - 1);
-            if (Tab.ValidPosition(position) && CanMove(position))
+            pos.DefineValues(PiecePosition.Rank + 1, PiecePosition.File - 1);
+            if (Tab.ValidPosition(pos) && CanMove(pos))
             {
-                mat[position.Rank, position.File] = true;
+                mat[pos.Rank, pos.File] = true;
             }
             //nw
-            position.DefineValues(PiecePosition.Rank - 1, PiecePosition.File - 1);
-            if (Tab.ValidPosition(position) && CanMove(position))
+            pos.DefineValues(PiecePosition.Rank - 1, PiecePosition.File - 1);
+            if (Tab.ValidPosition(pos) && CanMove(pos))
             {
-                mat[position.Rank, position.File] = true;
+                mat[pos.Rank, pos.File] = true;
             }
 
 
             // #Special Move King Side Castle
             if (NumberOfMoves == 0 && !_match.Check)
             {
-                Position KingSideRook = new Position(position.Rank, position.File + 3);
+                Position KingSideRook = new Position(PiecePosition.Rank, PiecePosition.File + 3);
                 if (CastleTest(KingSideRook))
                 {
-                    Position p1 = new Position(position.Rank, position.File + 1);
-                    Position p2 = new Position(position.Rank, position.File + 2);
+                    Position p1 = new Position(PiecePosition.Rank, PiecePosition.File + 1);
+                    Position p2 = new Position(PiecePosition.Rank, PiecePosition.File + 2);
                     if (Tab.Piece(p1) == null && Tab.Piece(p2) == null)
                     {
-                        mat[position.Rank, position.File + 2] = true;
+                        mat[pos.Rank, pos.File + 2] = true;
                     }
                 }
-            }
-
-            // #Special Move Queen Side Castle
-            if (NumberOfMoves == 0 && !_match.Check)
-            {
-                Position QueenSideRook = new Position(position.Rank, position.File - 4);
+                // #Special Move Queen Side Castle
+                Position QueenSideRook = new Position(PiecePosition.Rank, PiecePosition.File - 4);
                 if (CastleTest(QueenSideRook))
                 {
-                    Position p1 = new Position(position.Rank, position.File - 1);
-                    Position p2 = new Position(position.Rank, position.File - 2);
-                    Position p3 = new Position(position.Rank, position.File - 3);
+                    Position p1 = new Position(PiecePosition.Rank, PiecePosition.File - 1);
+                    Position p2 = new Position(PiecePosition.Rank, PiecePosition.File - 2);
+                    Position p3 = new Position(PiecePosition.Rank, PiecePosition.File - 3);
                     if (Tab.Piece(p1) == null && Tab.Piece(p2) == null && Tab.Piece(p3) == null)
                     {
-                        mat[position.Rank, position.File - 2] = true;
+                        mat[pos.Rank, pos.File - 2] = true;
                     }
                 }
             }
